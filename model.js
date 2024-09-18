554885693ddbd5d57303b168c66beecd0601c56f
mariosuperthanos <64190306+mariosuperthanos@users.noreply.github.com>
2024-09-18 19:21:51 +0300

    Create model.js

---
+++ model.js
@@ -0,0 +1,61 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+  }
+};
+// myModule.js
+export let x = 0;
+
+export const loadRecipe = async function(id) {
+  try{
+    const data = await getJSON(`${API_URL}${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    };
+    // console.log(state.recipe);
+
+    return state.recipe;
+  } catch(err){
+    console.error(`${err} !!!`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function(query) {
+  try{
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    // the REINTERPRETATION of data
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    console.log(state.search.results);
+
+  } catch(err) {
+    console.error(`${err} !!!`);
+    throw err;
+  }
+};
+loadSearchResults('pizza');
