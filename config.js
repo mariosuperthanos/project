6a8194f65d384c6479dfe7234fbfeabc2c349a18
mariosuperthanos <64190306+mariosuperthanos@users.noreply.github.com>
2024-09-18 19:20:54 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// this var is uppercase because it represents a const that doesn't change
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
